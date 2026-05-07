846b93a12b0ea31ff96da8112787537d8f71f22a
Christian Ibsen <christian.ibsen.htx@gmail.com>
2026-05-07 12:06:21 +0200

    Added some tags

--- TicTacToe/Assets/Scripts/RoseClickScript.cs
+++ TicTacToe/Assets/Scripts/RoseClickScript.cs
@@ -1,16 +1,48 @@
+using Mono.Cecil.Cil;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
 
 public class RoseClickScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Color mouseOverColor = Color.blue;
+    private Color originalColor = Color.yellow;
+    private bool dragging = false;
+    private float distance;
+    private Vector3 startDist;
+
+
+    void OnMouseEnter()
     {
-        
+        GetComponent<Renderer>().material.color = mouseOverColor;
     }
 
-    // Update is called once per frame
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = originalColor;
+    }
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 rayPoint = ray.GetPoint(distance);
+        startDist = transform.position - rayPoint;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+
     void Update()
     {
-        
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint + startDist;
+        }
     }
 }
