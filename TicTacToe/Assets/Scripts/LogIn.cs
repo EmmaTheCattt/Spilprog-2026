af8f1828fb7ea18f068a025d2f54e80b4c54e52e
camiriise <camilla.riise.nielsen@gmail.com>
2026-05-08 20:57:13 +0200

    database linked with login

---
+++ TicTacToe/Assets/Scripts/LogIn.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class LogIn : MonoBehaviour
+{
+    public TMP_InputField name;
+    public TMP_InputField code;
+    public int baseRating = 1000;
+
+    public SQL db;
+    public GameObject sqldb;
+
+
+    public void SignUp()
+    {
+        string username = name.text;
+        string password = code.text;
+        db.CreatePlayer(username, password, baseRating);
+
+    }
+
+    public void PlayerLogIn()
+    {
+        string username = name.text;
+        string password = code.text;
+        db.GetRating(username, password);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        sqldb = GameObject.Find("DatabaseTest");
+        db = sqldb.GetComponent<SQL>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
