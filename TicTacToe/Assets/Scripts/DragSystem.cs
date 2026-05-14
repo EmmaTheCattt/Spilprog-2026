e740288121569f719156b81d8d1e6ad08014c332
Valdemar_S <valdemarstidsen@gmail.com>
2026-05-14 12:38:54 +0200

    sad boga noise

--- TicTacToe/Assets/Scripts/DragSystem.cs
+++ TicTacToe/Assets/Scripts/DragSystem.cs
@@ -1,14 +1,34 @@
 using UnityEngine;
 using Unity.Netcode;
+using Unity.Mathematics;
 
 public class DragSystem : NetworkBehaviour
 {
-
+    private float dx;
+    private float dy;
+    private void Update()
+    {
+        if (dx != 0 || dy != 0)
+        {
+                MoveServerRpc(dx, dy);
+        }
+            
+    }
+    [Rpc(SendTo.Server)]
+    private void MoveServerRpc(float dx, float dy)
+    {
+        transform.position += new Vector3(dx, dy, 0);
+    }
     private void OnMouseDrag()
     {
        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
-       transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+       //transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
        transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+       Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+       dx = worldMousePosition.x - transform.position.x;
+       dy = worldMousePosition.y - transform.position.y;
+       transform.position = new Vector3(worldMousePosition.x, worldMousePosition.y, 0);
+
        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
        spriteRenderer.sortingOrder = 2; // Sætter sorting order til 1 for at sikre, at det trækker over andre objekter
        BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
